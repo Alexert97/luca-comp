e6901afcb9e9401d517c5ab2f8473da8031ea0da
Alexert97 <38686550+Alexert97@users.noreply.github.com>
2018-12-08 09:36:56 +0100

    l
    l

--- Luca/WebContent/Timer.js
+++ Luca/WebContent/Timer.js
@@ -1,24 +1,22 @@
-
-
 const year = (new Date().getFullYear());
-const fourthOfJuly = new Date(year, 11, 7).getTime();
+const fourthOfJuly = new Date(year, 11, 19).getTime();
 
 
-let timer = setInterval(function(a) {
+let timer = setInterval(function (a) {
 
-	// get today's date
-	const today = new Date().getTime();
+    // get today's date
+    const today = new Date().getTime();
 
-	// get the difference
-	const diff = fourthOfJuly - today;
+    // get the difference
+    const diff = fourthOfJuly - today;
 
-	// math
-	let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-	let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-	let seconds = Math.floor((diff % (1000 * 60)) / 1000);
-		if (diff>=0){
-	// display
+    // math
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    if (diff >= 0) {
+        // display
         document.getElementById("timer").innerHTML =
             "<div class=\"days\"> \
             <div class=\"numbers\">" + days + "</div>days</div> \
@@ -28,18 +26,26 @@ let timer = setInterval(function(a) {
 		<div class=\"numbers\">" + minutes + "</div>minutes</div> \
 		<div class=\"seconds\"> \
 		<div class=\"numbers\">" + seconds + "</div>seconds</div> \
-		</div>";}
-		else {
-			document.getElementById("timer").innerHTML =
-				"<div class='video'>" +
-				"<iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/V10EBvSti2A\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>" +
-                "</div>"
-			document.getElementById("buoncomp").innerHTML =
-				"<div class='container'>" +
-				"<p>Buon <br> Compleanno <br> Luca!</p>" +
-				"</div>"
-			clearInterval(timer);
-		}
+		</div>"
+        document.getElementById("frase").innerHTML =
+            "<div class='frase'>" +
+            "<p>Tra</p>" +
+            "</div>"
+        document.getElementById("frase1").innerHTML =
+            "<div class='frase1'>" +
+            "<p>Sarai Penalmente Perseguibile!</p>" +
+            "</div>";
+    } else {
+        document.getElementById("timer").innerHTML =
+            "<div class='video'>" +
+            "<iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/V10EBvSti2A\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\"  allowfullscreen></iframe>" +
+            "</div>"
+        document.getElementById("buoncomp").innerHTML =
+            "<div class='container'>" +
+            "<p>Buon <br> Compleanno <br> Luca!</p>" +
+            "</div>"
+        clearInterval(timer);
+    }
 
 }, 1000);
 
